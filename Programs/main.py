08d12a82103545c5381a2029d5732859d5e7263b
Armandosnt <148914566+Armandosnt@users.noreply.github.com>
2024-06-21 01:50:20 +0500

    Delete Programs directory

--- Programs/main.py
+++
@@ -1,8 +0,0 @@
-my_set = {1, 2, 3, 4, 5, "CS", "John"} 
-
-print("До обновления: ", my_set ) 
-
-data = input("Введите данные через пробел: ").split()
-data = set(data)
-data.update(my_set)
-print(data)
