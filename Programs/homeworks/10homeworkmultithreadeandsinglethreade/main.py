d3daf1b2ebfb156ffbd13474dbc4883aa350a995
Armandosnt <148914566+Armandosnt@users.noreply.github.com>
2024-07-24 21:57:10 +0500

    Homework10

---
+++ Programs/homeworks/10homeworkmultithreadeandsinglethreade/main.py
@@ -0,0 +1,127 @@
+# from multiprocessing import Process
+
+# def f(name):
+#     print('hello',name)
+    
+# if __name__ == '__main__':
+#     p = Process(target=f, args=('bob',))
+#     ghfhfg
+#     ghfhfghfg
+    
+#     fghfghh
+#     fghfgh
+
+
+# from multiprocessing import Process
+
+# def create_text(text):
+#     with open(f"{text}.txt", "w") as file:
+#          file.write(text)
+         
+#  if __name__ == "__main__":
+#      for in in range(100):
+#          process = Process         
+
+# from multiprocessing import Process, Queue
+
+# def sender(queue):
+#     data = [1,2,3,4,5]
+#     for i in data:
+#         queue.put(i)
+        
+# def receiver(queue):
+#     while not queue.empty():
+#         print(queue.get())
+        
+# if __name__ == "__main__":
+#     q = Queue()
+#     process_1 = Process(target=sender, args=(q,))
+#     process_2 = Process(target=receiver, args=(q,))     
+   
+#     process_1.start()
+#     process_1.join()     
+    
+#     process_2.start()
+#     process_2.join()   
+
+
+# from multiprocessing import Process, Pipe
+# def get_data(conn):
+#     data = [1,2,3,4,5]
+#     conn.send(data)
+    
+# if __name__ == "__main__":
+#     blue, green = Pipe()  
+
+# import requests
+# from multiprocessing import Process
+# import threading
+
+# def download_image(url):
+#     response = requests.get(url)
+    
+#     if response.status_code == 200:
+#         with open('image.jpg','wb') as file:
+#          file.write(response.content)
+#          print("Downloaded")
+#     else:
+#         print("Error")
+         
+# if __name__ == "__main__":
+#     for i in range(1000):
+#         thread = threading.Thread(target=download_image, args={str(i)})
+#         thread.start()
+#         download_image("https://i.pinimg.com/236x/24/15/21/24152197af38deb718eb730992d441d0.jpg")
+  
+import requests
+from multiprocessing import Process
+import threading
+import time
+
+import threading
+import time
+
+
+def create_text(text):
+    with open(f"{text}.txt", "w") as file:
+      file.write(text)
+
+threads = []
+if __name__ == '__main__':
+    start = time.time()
+    print(start)
+    for i in range(100):
+        thread = threading.Thread(target=create_text, args={str(i)})
+        thread.start()
+        
+    end = time.time()
+    print(f"Execution time = {end-start}")   
+     
+
+    # if __name__ == "__main__":
+    #   for i in range(100):
+    #     thread = threading.Thread(target=create_text, args={str(i)})
+    #     thread.start()
+    #     time.sleep(1)
+
+
+    
+    
+# import threading
+# import time
+
+# def cre(x, y):
+#     time.sleep(1)
+#     print(x + y)
+
+
+# threads = []
+# if __name__ == '__main__':
+#     start = time.time()
+#     print(start)
+#     for i in range(100):
+#         thread = threading.Thread(target=sum, args=[i,i])
+#         thread.start()
+        
+#     end = time.time()
+#     print(f"Execution time = {end-start}")
